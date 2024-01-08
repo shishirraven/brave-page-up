a8f9edf143c0df69822d204e19945640c605f823
webfort-neeraj <132546063+webfort-neeraj@users.noreply.github.com>
2024-01-08 17:21:30 +0530

    5th commit

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e5c7ffafd4943f60a551');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f1ee4a20b2207bb36469');
