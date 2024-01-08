5df0a01573c8e0c351bc9139acc372fecb7818ea
webfort-neeraj <132546063+webfort-neeraj@users.noreply.github.com>
2024-01-08 11:29:14 +0530

    2nd commit

--- src/render.php
+++ src/render.php
@@ -6,6 +6,7 @@
 
 <div id="scroll-to-top-container" style="bottom: <?php echo $attributes['posUpDown']; ?>px; <?php echo $attributes['posLeftRight'] . ': ' . $attributes['posLeftRightPx']; ?>px" <?php echo get_block_wrapper_attributes(); ?>>
     <!-- Your existing content goes here -->
-    <?php echo $content; ?>
+    <?php echo wp_kses_post( html_entity_decode( $content ) ); ?>
 </div>
+
 
